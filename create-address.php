632c16eee6dc569b2bcce57c2e92a3ef575ccadf
MLD-pro <morgan.ld.pro@gmail.com>
2025-06-11 15:22:11 +0000

    étape 6 : create address

---
+++ create-address.php
@@ -0,0 +1,15 @@
+<?php 
+
+require "connexion.php";
+
+$query = $db->prepare("INSERT INTO address (id, street, city, zipcode) VALUES (NULL, :street, :city, :zipcode)");
+
+$parameters = [
+    'street' => $_POST['street'],
+    'city' => $_POST['city'],
+    'zipcode' => $_POST['zipcode']
+];
+
+$query->execute($parameters);
+
+?>
